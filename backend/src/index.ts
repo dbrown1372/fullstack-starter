a7fed5cdd5b18b9c0662a30b83d5f3db6a807aac
dbrown1372 <davebrown_21@outlook.com>
2026-01-31 15:05:08 -0700

    update

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+	import express from "express";
+	import cors from "cors";
+	 
+	const app = express();
+	app.use(cors());
+	app.use(express.json());
+	 
+	app.get("/", (req, res) => {
+	  res.json({ message: "API running 🚀" });
+	});
+	 
+	app.listen(3000, () => console.log("Server running on port 3000"));
+	
+	Add Scripts in package.json;
+	 
+	"scripts";
+	 {
+	  "dev"; "nodemon src/index.ts"
+	}
