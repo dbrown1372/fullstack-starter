e4f0037f20fdda0e2348a519eb96e5fa35b6a6ed
Adams-Chaz <adams.chaz.m@gmail.com>
2026-02-02 11:31:12 -0700

    Fixed package.json formatting; Fixed server;

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,12 @@
-	import express from "express";
-	import cors from "cors";
-	 
-	const app = express();
-	app.use(cors());
-	app.use(express.json());
-	 
-	app.get("/", (req, res) => {
-	  res.json({ message: "API running 🚀" });
-	});
-	 
-	app.listen(3000, () => console.log("Server running on port 3000"));
+import express from "express";
+import cors from "cors";
 	
-	Add Scripts in package.json;
-	 
-	"scripts";
-	 {
-	  "dev"; "nodemon src/index.ts"
-	}
+const app = express();
+app.use(cors());
+app.use(express.json());
+	
+app.get("/", (req, res) => {
+	res.json({ message: "API running 🚀" });
+});
+	
+app.listen(3000, () => console.log("Server running on port 3000"));
